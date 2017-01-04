af0e22a49bb5880e7d2f55bd10faa053725e7dab
Michal Švácha <michalsvacha@michal-mbp-2.home>
2017-01-04 19:33:14 +0100

    task/Add entry fields in the model

--- URLRemote/Classes/Model/Entry.swift
+++ URLRemote/Classes/Model/Entry.swift
@@ -7,8 +7,32 @@
 //
 
 import Foundation
+import ObjectMapper
 
 ///
-class Entry {
-    ///
+enum EntryType: Int {
+    case Custom = 0
+    case SimpleHTTP = 1
+    case Quido = 2
+}
+
+///
+class Entry: Mappable {
+    var color: UInt32?
+    var icon: String?
+    var url: String?
+    var type: EntryType?
+    
+    init() {}
+    
+    // MARK: - ObjectMapper methods
+    
+    required init?(map: Map) {}
+    
+    func mapping(map: Map) {
+        color <- map["color"]
+        icon <- map["icon"]
+        url <- map["url"]
+        type <- map["type"]
+    }
 }
---
+++ URLRemote/Classes/ViewModel/EntrySetupViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  EntrySetupViewModel.swift
+//  URLRemote
+//
+//  Created by Michal Švácha on 04/01/17.
+//  Copyright © 2017 Svacha, Michal. All rights reserved.
+//
+
+import Foundation
+
+class EntrySetupViewModel {
+    ///
+}
