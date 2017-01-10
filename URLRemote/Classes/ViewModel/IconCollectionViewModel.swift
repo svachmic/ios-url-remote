bf8d6d0e0a7afab22a034011536ff63cd465c63f
Michal Švácha <michalsvacha@michal-mbp-2.home>
2017-01-10 22:11:09 +0100

    Icon collection ViewModel initial setup

---
+++ URLRemote/Classes/ViewModel/IconCollectionViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  IconCollectionViewModel.swift
+//  URLRemote
+//
+//  Created by Michal Švácha on 10/01/17.
+//  Copyright © 2017 Svacha, Michal. All rights reserved.
+//
+
+import Foundation
+import Bond
+
+///
+class IconCollectionViewModel {
+    ///
+    let contents = MutableObservableArray<String>([
+        "lightbulb",
+        "lock_on",
+        "play"
+        ])
+}
