8d0e8f5f4d39b919ff5b0c82f0a3bbc57ffc116b
Michal Švácha <michal.svacha@gmail.com>
2017-01-12 20:59:06 +0100

    Added all image assets to the viewModel

--- URLRemote/Classes/ViewModel/IconCollectionViewModel.swift
+++ URLRemote/Classes/ViewModel/IconCollectionViewModel.swift
@@ -9,12 +9,40 @@
 import Foundation
 import Bond
 
+
 ///
 class IconCollectionViewModel {
     ///
-    let contents = MutableObservableArray<String>([
-        "lightbulb_on",
-        "lock_on",
-        "play"
-        ])
+    
+    let general = Observable2DArraySection<String, String>(
+        metadata: NSLocalizedString("GENERAL", comment: ""),
+        items: ["house_1", "house_2", "lock_on", "lock_off", "plus", "minus", "on", "off"]
+    )
+    
+    let player = Observable2DArraySection<String, String>(
+        metadata: NSLocalizedString("PLAYER", comment: ""),
+        items: ["play", "pause", "stop", "rew", "fwd", "previous", "next"]
+    )
+    
+    let lights = Observable2DArraySection<String, String>(
+        metadata: NSLocalizedString("LIGHTS", comment: ""),
+        items: ["lightbulb_on", "lightbulb_off", "day", "night"]
+    )
+    
+    let arrows = Observable2DArraySection<String, String>(
+        metadata: NSLocalizedString("ARROWS", comment: ""),
+        items: ["up", "down", "left", "right", "double_up", "double_down", "double_left", "double_right"]
+    )
+    
+    let contents = MutableObservable2DArray<String, String>([])
+    
+    var initialSelection = Observable<IndexPath?>(nil)
+    var userSelection = Observable<IndexPath?>(nil)
+    
+    init() {
+        self.contents.appendSection(general)
+        self.contents.appendSection(player)
+        self.contents.appendSection(lights)
+        self.contents.appendSection(arrows)
+    }
 }
