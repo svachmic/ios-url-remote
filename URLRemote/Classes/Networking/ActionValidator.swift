d65b13f4c43199baff7d338022cc98860ba2ab3c
Michal Švácha <michal.svacha@gmail.com>
2017-01-20 19:32:05 +0100

    ActionValidator protocol for validating output.

---
+++ URLRemote/Classes/Networking/ActionValidator.swift
@@ -0,0 +1,19 @@
+//
+//  ActionValidator.swift
+//  URLRemote
+//
+//  Created by Michal Švácha on 20/01/17.
+//  Copyright © 2017 Svacha, Michal. All rights reserved.
+//
+
+import Foundation
+
+/// Protocol for output validation to be implemented for various EntryTypes.
+protocol ActionValidator {
+    
+    /// Validates the performed action and returns the outcome.
+    ///
+    /// - Parameter output: The string given on the output.
+    /// - Returns: A flag indicating whether or not it was a valid action.
+    func validateOutput(output: String) -> Bool
+}
