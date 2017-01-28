6b551df85807aef16e761718e03bd316edc95c7d
Michal Švácha <michal.svacha@gmail.com>
2017-01-28 18:05:48 +0100

    Returns SimpleHTTPValidator when type is not specified

--- URLRemote/Classes/Networking/ValidatorFactory.swift
+++ URLRemote/Classes/Networking/ValidatorFactory.swift
@@ -8,21 +8,22 @@
 
 import Foundation
 
-///
+/// Factory for creating specific Validator instances for the type of the Entry.
 class ValidatorFactory {
     
+    /// Returns a specific Validator for the Entry. If no type is specified, SimpleHTTPValidator is created.
     ///
+    /// - Parameter entry: Entry for which the Validator should be created.
+    /// - Returns: The specific Validator for the Entry.
     class func validator(for entry: Entry) -> Validator {
-        guard let type = entry.type else {
-            return GenericValidator(with: "")
-        }
-        
-        switch type {
-        case .SimpleHTTP:
+        switch entry.type {
+        case .none:
             return SimpleHTTPValidator()
-        case .Quido:
+        case .some(.SimpleHTTP):
+            return SimpleHTTPValidator()
+        case .some(.Quido):
             return QuidoValidator()
-        case .Custom:
+        case .some(.Custom):
             return CustomCriteriaValidator(with: entry.customCriteria)
         }
     }
