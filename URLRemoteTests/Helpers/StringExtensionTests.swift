a7be0f04bf8e49be2b7a3770288afaf5a4d7d72f
Michal Švácha <michal.svacha@gmail.com>
2017-02-05 16:23:35 +0100

    String Extension tests init

---
+++ URLRemoteTests/Helpers/StringExtensionTests.swift
@@ -0,0 +1,29 @@
+//
+//  StringExtensionTests.swift
+//  URLRemote
+//
+//  Created by Michal Švácha on 05/02/17.
+//  Copyright © 2017 Svacha, Michal. All rights reserved.
+//
+
+import XCTest
+@testable import URLRemote
+
+class StringExtensionTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    func testEmailValidator() {
+        ///
+    }
+    
+    func testURLValidator() {
+        ///
+    }
+}
