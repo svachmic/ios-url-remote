cd386675b6a76f42881d3ff453b81c43d171ad3d
Michal Švácha <michal.svacha@gmail.com>
2017-02-14 20:39:22 +0100

    Added tests for array extension

---
+++ URLRemoteTests/Helpers/ArrayExtensionTests.swift
@@ -0,0 +1,32 @@
+//
+//  ArrayExtensionTests.swift
+//  URLRemote
+//
+//  Created by Michal Švácha on 14/02/17.
+//  Copyright © 2017 Svacha, Michal. All rights reserved.
+//
+
+import XCTest
+
+class ArrayExtensionTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    /// Tests subscript and returning valid values instead of crashing.
+    func testSafeSubscript() {
+        var array = [0, 1]
+        XCTAssertNotNil(array[safe: 0])
+        XCTAssertNotNil(array[safe: 1])
+        XCTAssertNil(array[safe: 2])
+        
+        array.removeAll()
+        XCTAssertNil(array[safe: 0])
+    }
+    
+}
