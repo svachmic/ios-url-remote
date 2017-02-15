59de3c59bd2d9d0d36280c4aea7348f0e91821be
Michal Švácha <michal.svacha@gmail.com>
2017-02-15 21:16:33 +0100

    Added enum collection tests

---
+++ URLRemoteTests/EnumCollectionTests.swift
@@ -0,0 +1,45 @@
+//
+//  EnumCollectionTests.swift
+//  URLRemote
+//
+//  Created by Michal Švácha on 15/02/17.
+//  Copyright © 2017 Svacha, Michal. All rights reserved.
+//
+
+import XCTest
+
+class EnumCollectionTests: XCTestCase {
+    
+    /// Test enum for the purposes of the test
+    enum TestEnum: EnumCollection {
+        case first
+        case second
+        case third
+    }
+    
+    var collection: [TestEnum] = []
+    
+    override func setUp() {
+        super.setUp()
+        collection = TestEnum.allValues
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    /// Tests presence of all enum values in an enum allValues collection.
+    func testPresence() {
+        XCTAssertTrue(collection.count == 3)
+        XCTAssertTrue(collection.contains(.first))
+        XCTAssertTrue(collection.contains(.second))
+        XCTAssertTrue(collection.contains(.third))
+    }
+    
+    /// Tests order of the enum values - should be the same as defined.
+    func testOrder() {
+        XCTAssertEqual(collection[0], .first)
+        XCTAssertEqual(collection[1], .second)
+        XCTAssertEqual(collection[2], .third)
+    }
+}
