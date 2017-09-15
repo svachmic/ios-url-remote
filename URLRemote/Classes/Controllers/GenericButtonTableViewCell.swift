ab704bdb14c5c3ba5c2fb1ce6be7619b4bf97405
Michal Švácha <michal.svacha@gmail.com>
2017-09-15 12:33:42 +0200

    bug/#18 Fix memory leaks

--- URLRemote/Classes/Controllers/GenericButtonTableViewCell.swift
+++ URLRemote/Classes/Controllers/GenericButtonTableViewCell.swift
@@ -9,29 +9,21 @@
 import UIKit
 import Material
 
-/// View for a cell representing the type of an entry. Drawn simply as a text with currently selected type and an arrow indicating interaction with the cell.
+/// View responsible for displaying some information (type, category, etc.) of an entry that can be changed by a press of the button.
 class GenericButtonTableViewCell: UITableViewCell {
-    var button: FlatButton?
+    @IBOutlet weak var button: FlatButton!
+    @IBOutlet weak var arrowImageView: UIImageView!
 
     override func awakeFromNib() {
         super.awakeFromNib()
-    }
-    
-    /// Has to be called in order to set up all the subviews. Container with tag 10 is otherwise nil before the super method is called.
-    override func layoutSubviews() {
-        super.layoutSubviews()
         
-        let container = self.viewWithTag(10)!
+        button.titleColor = .gray
+        button.titleLabel?.font = RobotoFont.bold(with: 13)
+        button.contentHorizontalAlignment = .left
+        button.titleEdgeInsets = UIEdgeInsets(top: 0, left: 10.0, bottom: 0, right: 0)
         
-        self.button = container.viewWithTag(1) as? FlatButton
-        self.button?.titleColor = .gray
-        self.button?.titleLabel?.font = RobotoFont.bold(with: 13)
-        self.button?.contentHorizontalAlignment = .left
-        self.button?.titleEdgeInsets = UIEdgeInsets(top: 0, left: 10.0, bottom: 0, right: 0)
-        
-        let imageView = self.viewWithTag(2) as? UIImageView
-        imageView?.image = UIImage(named: "arrow_right")!.withRenderingMode(.alwaysTemplate)
-        imageView?.tintColor = .gray
+        arrowImageView.image = UIImage(named: "arrow_right")!.withRenderingMode(.alwaysTemplate)
+        arrowImageView.tintColor = .gray
         
         self.selectionStyle = .none
     }
